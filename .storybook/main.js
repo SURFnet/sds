2ca5e8471b408e64e17f433aea0b58f8b534c4a0
Okke Harsta <oharsta@zilverline.com>
2023-04-23 09:19:13 +0200

    Migrated to latest version of storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,83 +1,67 @@
 const path = require('path');
 const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
-const toPath = (_path) => path.join(process.cwd(), _path);
-
+const toPath = _path => path.join(process.cwd(), _path);
 module.exports = {
-    "stories": [
-        "../src/**/*.stories.mdx",
-        "../src/**/*.stories.@(js|jsx|ts|tsx)"
-    ],
-    "addons": [
-        "@storybook/addon-links",
-        "@storybook/addon-essentials",
-        "@storybook/preset-scss",
-        "@storybook/addon-interactions",
-    ],
-    "core": {
-        "builder": "webpack5"
-    },
-    features: {
-        interactionsDebugger: true,
-        buildStoriesJson: true
-    },
-    webpackFinal: async (config) => {
-        // configure for absolute imports
-        config.resolve.plugins = [
-            ...(config.resolve.plugins || []),
-            new TsconfigPathsPlugin({
-                extensions: config.resolve.extensions,
-            }),
-        ];
+  "stories": ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+  "addons": ["@storybook/addon-links", "@storybook/addon-essentials", "@storybook/preset-scss", "@storybook/addon-interactions", '@storybook/addon-mdx-gfm'],
+  features: {
+    interactionsDebugger: true,
+    buildStoriesJson: true
+  },
+  webpackFinal: async config => {
+    // configure for absolute imports
+    config.resolve.plugins = [...(config.resolve.plugins || []), new TsconfigPathsPlugin({
+      extensions: config.resolve.extensions
+    })];
 
-        // disable whatever is already set to load SVGs
-        config.module.rules
-            .filter(rule => rule.test.test('.svg'))
-            .forEach(rule => rule.exclude = /\.svg$/i);
+    // disable whatever is already set to load SVGs
+    // config.module.rules.filter(rule => rule.test.test('.svg')).forEach(rule => rule.exclude = /\.svg$/i);
 
-        // add SVGR instead
-        config.module.rules.push({
-            test: /\.svg$/,
-            use: [
-                {
-                    loader: '@svgr/webpack',
-                    options: {
-                        svgoConfig: {
-                            plugins: [
-                                {
-                                    name: 'preset-default',
-                                    params: {
-                                        overrides: {
-                                            removeViewBox: false,
-                                        },
-                                    },
-                                },
-                            ]
-                        },
-                    },
-                },
-                {
-                    loader: 'file-loader',
-                    options: {
-                        name: 'static/media/[path][name].[ext]'
-                    }
+    // add SVGR instead
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: [{
+        loader: '@svgr/webpack',
+        options: {
+          svgoConfig: {
+            plugins: [{
+              name: 'preset-default',
+              params: {
+                overrides: {
+                  removeViewBox: false
                 }
-            ],
-            type: 'javascript/auto',
-            issuer: {
-                and: [/\.(ts|tsx|js|jsx|md|mdx)$/]
-            }
-        });
-        return {
-            ...config,
-            resolve: {
-                ...config.resolve,
-                alias: {
-                    ...config.resolve.alias,
-                    '@emotion/core': toPath('node_modules/@emotion/react'),
-                    'emotion-theming': toPath('node_modules/@emotion/react'),
-                },
-            },
-        };
-
-    }
-}
+              }
+            }]
+          }
+        }
+      }, {
+        loader: 'file-loader',
+        options: {
+          name: 'static/media/[path][name].[ext]'
+        }
+      }],
+      type: 'javascript/auto',
+      issuer: {
+        and: [/\.(ts|tsx|js|jsx|md|mdx)$/]
+      }
+    });
+    return {
+      ...config,
+      resolve: {
+        ...config.resolve,
+        alias: {
+          ...config.resolve.alias,
+          '@emotion/core': toPath('node_modules/@emotion/react'),
+          'emotion-theming': toPath('node_modules/@emotion/react')
+        }
+      }
+    };
+  },
+  framework: {
+    name: '@storybook/react-webpack5',
+    options: {}
+  },
+  docs: {
+    autodocs: true
+  }
+};

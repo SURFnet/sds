88333fe2eada523f735c8c6b3e4deb18a7c21ae4
Okke Harsta <oharsta@zilverline.com>
2023-11-14 18:53:43 +0100

    Non expired logo

--- .storybook/StorybookSURF.js
+++ .storybook/StorybookSURF.js
@@ -6,6 +6,6 @@ export default create({
   base: 'light',
   brandTitle: 'SURF Design System',
   brandUrl: 'https://sds.surf.nl',
-  brandImage: 'https://zeroheight.s3.eu-west-1.amazonaws.com/zeroheight-huddle/logos/93434/110897/logo.png?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIA3AVNYHQK63XCNSNR%2F20231109%2Feu-west-1%2Fs3%2Faws4_request&X-Amz-Date=20231109T105411Z&X-Amz-Expires=86400&X-Amz-SignedHeaders=host&X-Amz-Signature=13544b338ea61cf75f3566137abe42973125d5eae3c7c35fdd31b5b04ea4c632',
+  brandImage: 'https://www.surf.nl/themes/surf/logo.svg',
   brandTarget: '_self',
 });

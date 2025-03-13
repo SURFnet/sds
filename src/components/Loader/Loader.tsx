c9fa2bdb82ce0a2b95d0868704033e5cee1fbab8
Okke Harsta <oharsta@zilverline.com>
2025-03-13 14:13:11 +0100

    Release 0.0.129

--- src/components/Loader/Loader.tsx
+++ src/components/Loader/Loader.tsx
@@ -1,12 +1,15 @@
 import React from "react";
 import "./Loader.scss";
+import LogoColor from "../Logo/LogoColor";
 
-export interface LoaderProps {}
+export interface LoaderProps {
+    className?: string,
+}
 
 const Loader = (props: React.PropsWithChildren<LoaderProps>) => {
-
+    const className = `sds--loading-container ${props.className || ""}`
     return (
-        <div className="sds--loading-container">
+        <div className={className}>
             <div className="sds--loading">
                 <span className="sds--loading--dot"/>
                 <span className="sds--loading--dot"/>
